ad9aed2826fab143e25d7912b85037b5534ea49e
Aron Adler <aronadler@hotmail.com>
2017-06-15 16:19:31 +0100

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public', 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2017'],
+                plugins: ['transform-class-properties']
+            }
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+        publicPath: "/dist/",
+        compress: true,
+        port: 9000
+    }
+
+};
