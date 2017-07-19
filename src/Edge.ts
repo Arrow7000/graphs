9aa5689e99f93f27178d1b2c5af70b7eaec3a606
Aron Adler <aronadler@hotmail.com>
2017-07-19 09:41:22 +0100

    Add logic for rendering directed edges

--- src/Edge.ts
+++ src/Edge.ts
@@ -1,6 +1,6 @@
 import Vertex from './Vertex';
-import { getDistance } from './Point';
-import { edgeColour } from "./config";
+import P, { getDistance } from './Point';
+import { edgeColour, vertexRadius } from "./config";
 
 // const arrowHeadLen = 5;
 
@@ -35,12 +35,42 @@ class Edge {
         ctx.moveTo(a.position.x, a.position.y);
         ctx.lineTo(b.position.x, b.position.y);
         ctx.strokeStyle = edgeColour;
+        ctx.stroke();
+
+
+
 
         if (this.directed) {
+            const { a, b } = this.vertices;
+            const { x, y } = b.position;
+            const lineVec = a.position.vecTo(b.position);
+            const angle = lineVec.getAngle() + 90;
 
+            const sideLen = 15;
+            const center = this.vertices.b.position;
+
+            const triangleTip = center.add(new P(0, vertexRadius).rotate(angle));
+
+            const vertical = new P(0, sideLen);
+            const rotationDegrees = 30;
+            const toA = vertical.rotate(rotationDegrees);
+            const toB = vertical.rotate(-rotationDegrees);
+
+            const triangleA = triangleTip.add(toA).rotate(angle, triangleTip);
+            const triangleB = triangleTip.add(toB).rotate(angle, triangleTip);
+
+            ctx.beginPath();
+            ctx.moveTo(triangleTip.x, triangleTip.y);
+
+            ctx.lineWidth = 3;
+            ctx.fillStyle = edgeColour;
+            ctx.lineTo(triangleA.x, triangleA.y);
+            ctx.lineTo(triangleB.x, triangleB.y);
+            ctx.lineTo(triangleTip.x, triangleTip.y);
+
+            ctx.fill();
         }
 
-        ctx.stroke();
     }
 }
 
