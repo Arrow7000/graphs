c90c3ca1886962d6d39a98c4a5dbb6ff10941c5e
Aron Adler <aron@smartowldesign.com>
2017-10-02 23:18:30 +0100

    Add CSS and Sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,52 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'public', 'dist'),
-        filename: 'bundle.js'
-    },
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "public", "dist"),
+    filename: "bundle.js"
+  },
 
-    devtool: "source-map",
+  devtool: "source-map",
 
-    module: {
-        loaders: [{
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2017'],
-                    plugins: ['transform-class-properties', 'transform-object-rest-spread']
-                }
-            },
-            {
-                test: /\.ts$/,
-                loader: 'awesome-typescript-loader'
-            },
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        query: {
+          presets: ["es2017"],
+          plugins: [
+            "transform-class-properties",
+            "transform-object-rest-spread"
+          ]
+        }
+      },
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
         ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        publicPath: "/dist/",
-        compress: true,
-        port: process.env.PORT || 9000,
-        open: true
-    },
-    resolve: { extensions: ['.js', '.ts'] }
-
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    publicPath: "/dist/",
+    compress: true,
+    port: process.env.PORT || 9000,
+    open: true
+  },
+  resolve: { extensions: [".js", ".ts", ".css"] }
 };
