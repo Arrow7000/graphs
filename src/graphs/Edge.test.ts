a8b7b14ecf8d5279257ef66525a6836ca65bafab
Aron Adler <aronadler@hotmail.com>
2018-08-17 18:09:28 +0100

    Add Edge tests

--- src/graphs/Edge.test.ts
+++ src/graphs/Edge.test.ts
@@ -3,8 +3,75 @@ import Vertex from "./Vertex";
 import P from "../vectors/Point";
 
 describe("Edge", () => {
-  test("Create new edge", () => {
-    const v1 = new Vertex();
-    const e = new Edge(v1, new P());
+  let v0: Vertex, v1: Vertex;
+  let p1: P, p2: P;
+
+  beforeEach(() => {
+    p1 = new P(1, 1);
+    p2 = new P(2, 2);
+    v0 = new Vertex(new P(0, 0));
+    v1 = new Vertex(p1);
+  });
+
+  describe("Get length", () => {
+    test("Get length when unconnected", () => {
+      const e = new Edge(v0, p1);
+      expect(e.length).toBe(Math.SQRT2);
+    });
+
+    test("Get length when connected", () => {
+      const e = new Edge(v0, v1);
+      expect(e.length).toBe(Math.SQRT2);
+    });
+  });
+
+  describe("Set point B", () => {
+    test("Set point B when unconnected", () => {
+      const e = new Edge(v0, p1);
+      e.setPointB(p2);
+      expect(e.vertices.b).toEqual(p2);
+    });
+
+    test("When connected set point B should throw", () => {
+      const e = new Edge(v0, v1);
+      expect(() => e.setPointB(p2)).toThrow();
+    });
+  });
+
+  test("Attach to vertex", () => {
+    const e = new Edge(v0, p1);
+    e.attachToVertex(v1);
+    expect(e.to).toBe(v1);
+  });
+
+  describe("Is connected", () => {
+    test("When not connected", () => {
+      const e = new Edge(v0, p1);
+      expect(e.isConnected).toBe(false);
+    });
+
+    test("When connected", () => {
+      const e = new Edge(v0, v1);
+      expect(e.isConnected).toBe(true);
+    });
+  });
+
+  describe("Get from and to", () => {
+    test("From", () => {
+      const e = new Edge(v0, p1);
+      expect(e.from).toEqual(v0);
+    });
+
+    describe("To", () => {
+      test("When not connected", () => {
+        const e = new Edge(v0, p1);
+        expect(e.to).toBe(null);
+      });
+
+      test("When connected", () => {
+        const e = new Edge(v0, v1);
+        expect(e.to).toBe(v1);
+      });
+    });
   });
 });
--- src/graphs/Edge.ts
+++ src/graphs/Edge.ts
@@ -5,26 +5,24 @@ import { uuid } from "./helpers";
 
 class Edge {
   id: string;
-  directed: boolean;
+  // directed: boolean;
   vertices: {
     a: Vertex;
     b: Vertex | P;
   };
 
-  constructor(vertexA: Vertex, vertexB: Vertex | P, directed = true) {
+  constructor(vertexA: Vertex, vertexB: Vertex | P) {
     this.id = uuid();
     this.vertices = {
       a: vertexA,
       b: vertexB
     };
-    this.directed = directed;
   }
 
-  getLength() {
+  get length() {
     const { a, b } = this.vertices;
     const bPos = isP(b) ? b : b.position;
-    const distance = getDistance(a.position, bPos);
-    return distance;
+    return a.position.getDistance(bPos);
   }
 
   setPointB(point: P) {
@@ -50,51 +48,51 @@ class Edge {
     return this.isConnected ? <Vertex>this.vertices.b : null;
   }
 
-  render(ctx: CanvasRenderingContext2D) {
-    const { a, b } = this.vertices;
-    const bPos = isP(b) ? b : b.position;
+  // render(ctx: CanvasRenderingContext2D) {
+  //   const { a, b } = this.vertices;
+  //   const bPos = isP(b) ? b : b.position;
 
-    ctx.beginPath();
-    // ctx.lineWidth = 2;
-    ctx.lineWidth = edgeWidth;
-    ctx.lineCap = "round";
-    ctx.moveTo(a.position.x, a.position.y);
-    ctx.lineTo(bPos.x, bPos.y);
-    ctx.strokeStyle = edgeColour;
-    ctx.stroke();
+  //   ctx.beginPath();
+  //   // ctx.lineWidth = 2;
+  //   ctx.lineWidth = edgeWidth;
+  //   ctx.lineCap = "round";
+  //   ctx.moveTo(a.position.x, a.position.y);
+  //   ctx.lineTo(bPos.x, bPos.y);
+  //   ctx.strokeStyle = edgeColour;
+  //   ctx.stroke();
 
-    if (this.directed) {
-      const { a, b } = this.vertices;
-      const { x, y } = bPos;
-      const lineVec = a.position.vecTo(bPos);
-      const angle = lineVec.getAngle() + 90;
+  //   if (this.directed) {
+  //     const { a, b } = this.vertices;
+  //     const { x, y } = bPos;
+  //     const lineVec = a.position.vecTo(bPos);
+  //     const angle = lineVec.getAngle() + 90;
 
-      const sideLen = edgeWidth * 2;
-      const center = bPos;
+  //     const sideLen = edgeWidth * 2;
+  //     const center = bPos;
 
-      const triangleTip = center.add(
-        new P(0, vertexRadius + edgeWidth).rotate(angle)
-      );
+  //     const triangleTip = center.add(
+  //       new P(0, vertexRadius + edgeWidth).rotate(angle)
+  //     );
 
-      const vertical = new P(0, sideLen);
-      const rotationDegrees = 30;
-      const toA = vertical.rotate(rotationDegrees);
-      const toB = vertical.rotate(-rotationDegrees);
+  //     const vertical = new P(0, sideLen);
+  //     const rotationDegrees = 30;
+  //     const toA = vertical.rotate(rotationDegrees);
+  //     const toB = vertical.rotate(-rotationDegrees);
 
-      const triangleA = triangleTip.add(toA).rotate(angle, triangleTip);
-      const triangleB = triangleTip.add(toB).rotate(angle, triangleTip);
+  //     const triangleA = triangleTip.add(toA).rotate(angle, triangleTip);
+  //     const triangleB = triangleTip.add(toB).rotate(angle, triangleTip);
 
-      ctx.beginPath();
-      ctx.moveTo(triangleTip.x, triangleTip.y);
-      ctx.lineWidth = edgeWidth;
-      ctx.fillStyle = edgeColour;
-      ctx.lineTo(triangleA.x, triangleA.y);
-      ctx.lineTo(triangleB.x, triangleB.y);
-      ctx.lineTo(triangleTip.x, triangleTip.y);
+  //     ctx.beginPath();
+  //     ctx.moveTo(triangleTip.x, triangleTip.y);
+  //     ctx.lineWidth = edgeWidth;
+  //     ctx.fillStyle = edgeColour;
+  //     ctx.lineTo(triangleA.x, triangleA.y);
+  //     ctx.lineTo(triangleB.x, triangleB.y);
+  //     ctx.lineTo(triangleTip.x, triangleTip.y);
 
-      ctx.fill();
-    }
-  }
+  //     ctx.fill();
+  //   }
+  // }
 }
 
 export default Edge;
